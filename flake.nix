60ccb0f01d8268bd0123e9a1993a290b2ae88022
Jacob Logan <103975454+j-lgs@users.noreply.github.com>
2022-06-06 12:44:47 +1000

    build: add devshell to flake.nix
    flake.nix now just spanws a devshell with dependencies needed to
    hack on and test the project.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,52 @@
 {
-  description = "A flake for setting up a build environment for terraform-provider-talos";
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
-  outputs = { self, nixpkgs }:
-  let
-    version = builtins.substring 0 8 self.lastModifiedDate
-    supportedSystems = [ "x86_64-linux" ];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-  in {
-    packages =
-      let
-        pkgs = nixpkgsFor.x86_64-linux
-      in {
-        default = pkgs.buildGoModule {
-	  pname = "terraform-provider-talos";
-	  inherit version;
-	  src = ./.;
-	  vendorSha256 = pkgs.lib.fakeSha256;
-	}
-      };
+  description =
+    "A flake for setting up a build environment for terraform-provider-talos";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-22.05"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+    devshell = { url = "github:numtide/devshell"; };
   };
+  outputs = { self, nixpkgs, devshell, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
+        };
+
+        gopls = pkgs.gopls;
+        go = pkgs.go;
+        make = pkgs.coreutils;
+        qemu_full = pkgs.qemu_full;
+
+      in {
+        devShells = {
+          default = pkgs.devshell.mkShell {
+            name = "terraform-provider-talos";
+
+            packages = [ pkgs.go_1_18 gopls make qemu_full pkgs.inotify-tools ];
+            commands = [
+              {
+                name = "acctest";
+                category = "testing";
+                help = "Run acceptance tests.";
+                command ="tools/pretest.sh";
+              }
+              {
+                name ="test";
+                category = "testing";
+                help = "Run unit tests.";
+                command="go vet ./... && go run honnef.co/go/tools/cmd/staticcheck ./talos && go test -v -race -vet=off ./...";
+              }
+              {
+                name = "generate";
+                category = "code";
+                help = "Regenerate documentation.";
+                command ="go generate ./...";
+              }
+            ];
+          };
+        };
+      });
 }
